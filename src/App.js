61fee3f29038cb7a2031f7dd0976449ebe121cdc
Jailson Prado <jailson.dionisio@ibm.com>
2021-10-09 14:55:21 -0300

    parte 1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import Produto from './Produto';
-// Os links abaixo puxam dados de um produto em formato JSON
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// Crie uma interface com 3 botões, um para cada produto.
-// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-// Mostre apenas um produto por vez
-// Mostre a mensagem carregando... enquanto o fetch é realizado
 
 const App = () => {
-  const [dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(null);
+  const [contar, setContar] = React.useState(0);
 
-  async function handleClick(event) {
-    setCarregando(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
-    );
-    const json = await response.json();
-    setDados(json);
-    setCarregando(false);
-  }
+  React.useEffect(() => {
+    console.log('executou');
+  }, []);
 
+  React.useEffect(() => {
+    document.title = 'Total ' + contar;
+  }, [contar]);
   return (
     <>
-      <button style={{ margin: '10px' }} onClick={handleClick}>
-        notebook
-      </button>
-      <button style={{ margin: '10px' }} onClick={handleClick}>
-        smartphone
-      </button>
-      <button style={{ margin: '10px' }} onClick={handleClick}>
-        tablet
-      </button>
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados && <Produto dados={dados} />}
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
     </>
   );
 };

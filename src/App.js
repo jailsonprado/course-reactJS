c7b7828d1b7b606ea30ff7f3d0b8ea0b2392eb8a
Jailson Prado <jailson.dionisio@ibm.com>
2021-10-10 19:37:12 -0300

    iniciando modulo de formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import Produto from './Produto';
-import { GlobalStorage } from './GlobalContext';
 
 const App = () => {
-  return (
-    <GlobalStorage>
-      <Produto />
-    </GlobalStorage>
-  );
+  return <div></div>;
 };
 
 export default App;

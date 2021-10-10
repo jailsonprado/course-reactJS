5867e59a1bf418d962793bfd8247fb10e4bbd55d
Jailson Prado <jailson.dionisio@ibm.com>
2021-10-10 13:56:06 -0300

    enviando novos documentos

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import Produto from './Produto';
-// Os links abaixo puxam dados de um produto em formato JSON
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// Crie uma interface com 3 botões, um para cada produto.
-// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-// Mostre apenas um produto por vez
-// Mostre a mensagem carregando... enquanto o fetch é realizado
 
 const App = () => {
-  const [produto, setProduto] = React.useState(null);
+  const video = React.useRef();
+  console.log(video);
 
-  React.useEffect(() => {
-    const produtoLocal = window.localStorage.getItem('produto');
-    if (produtoLocal !== null) setProduto(produtoLocal);
-  }, []);
-
-  React.useEffect(() => {
-    if (produto !== null) window.localStorage.setItem('produto', produto);
-  }, [produto]);
-
-  function handleClick({ target }) {
-    setProduto(target.innerText);
-  }
   return (
     <>
-      <h1>Preferencia: {produto}</h1>
-      <button onClick={handleClick} style={{ marginRight: '10px' }}>
-        notebook
-      </button>
-      <button onClick={handleClick}>smartphone</button>
-      <Produto produto={produto} />
+      <div ref={video}>Teste</div>
     </>
   );
 };
